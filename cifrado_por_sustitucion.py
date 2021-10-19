d179f8cbb1c53aab155a52308ae729a64857e1de
Rivera <17040044@itsuruapan.edu.mx>
2021-10-18 22:31:45 -0500

    listo

---
+++ cifrado_por_sustitucion.py
@@ -0,0 +1,46 @@
+import random
+# recibe el mensaje del usuario
+mensaje_1 = ""
+# valores aleatorios para la clave
+valor = random.randint(100, 200)
+
+# encriptar el mensaje
+def encriptar(msg, clave):
+    # mensaje encriptado
+    encriptado = ""
+    for i in msg:
+        encriptado += chr(ord(i)+clave)
+    # generamos clave
+    clave += valor
+    clave = chr(clave)
+    # cocateno la clave con el mensaje
+    encriptado += clave
+    return encriptado
+
+# decodifica el mensaje
+def decodificar(msg):
+    # obtener clave de la cadena
+    clave = ord(msg[-1])-valor
+    # eliminamos la clave
+    msg = msg[:-1]
+    mensaje_2 = ""
+    for j in msg:
+        # guardamos el mensaje desencriptado
+        mensaje_2 += chr(ord(j)-clave)
+    # retornamos el mensaje desencriptado
+    return mensaje_2
+
+n = 1
+print(f"************************* CHAT WHOAMI {n} *********************** ")
+while(True):
+    # mensaje
+    mensaje_1 = input("Ingrese el mensaje >_ ")
+    msg_encriptado = encriptar(mensaje_1, random.randint(1, 10))
+    msg_decodificado = decodificar(msg_encriptado)
+    n+=1
+    print("\n"*3)
+    print(f"************************* CHAT WHOAMI {n} *********************** ")
+    print("--------------------------------------------------------------- ")
+    print(f"Mensaje encriptado --> {msg_encriptado}")
+    print("--------------------------------------------------------------- ")
+    print(f"Mensaje recibido --> {msg_decodificado}")
